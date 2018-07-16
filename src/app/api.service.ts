6a39b23e257c728659b2442a30dd80c9d69bc13f
carkod <carkodw@gmail.com>
2018-07-16 18:33:35 +0200

    proxy

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 const httpOptions = {
   headers: new HttpHeaders({
     
     'Content-Type':  'application/json',
-    
     // 'Authorization': 'my-auth-token'
   })
 };
@@ -20,14 +21,23 @@ export class ApiService {
   limit: number = 50;
   startTime?: any = '';
   endtime? : any = '';
-  candlestickUrl: string = `${environment.api.base + environment.api.candlestick}?symbol=${this.symbol}&interval=${this.interval}`;
+  candlestickUrl: string = `${environment.api.candlestick}?symbol=${this.symbol}&interval=${this.interval}&limit=${this.limit}`;
+  dataPoints;
+  openPrices;
 
   constructor(private http: HttpClient) {
-    
   }
 
-  getCandelstick() {
-    return this.http.get('https://api.binance.com/api/v1/klines?symbol=TRXETH&interval=30m');
+  getCandlestick(): Observable<any> {
+    this.dataPoints = this.http.get<any[]>(this.candlestickUrl).pipe(map((d, i) => {
+      this.openPrices = [];
+      this.openPrices.push(d[i][0]);
+      return this.openPrices;
+    }));
+    console.log(this.dataPoints)
+    
+    return this.dataPoints;
+    // return candlestick;
   }
 
 
--- src/app/candlestick/candlestick.component.ts
+++ src/app/candlestick/candlestick.component.ts
@@ -1,82 +1,106 @@
-import { Component, OnInit } from '@angular/core';
-import { dates, closePrices, highPrices, lowPrices, openPrices } from './mock.data';
+import { Component, OnInit, OnChanges } from '@angular/core';
+// import { dates, closePrices, highPrices, lowPrices, openPrices } from './mock.data';
+import { dates } from './mock.data';
 import { MovingAverageService } from './moving-average.service';
 import { ApiService } from '../api.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'candlestick',
   templateUrl: './candlestick.component.html',
   styleUrls: ['./candlestick.component.css']
 })
-export class CandlestickComponent implements OnInit {
+export class CandlestickComponent implements OnInit, OnChanges {
 
   graph: object;
   maLineY: Array<any>;
   maLineX: Array<any>;
+  allDataPoints: Array<any> = [];
+  openPrices: Array<any> = [];
+  closePrices: Array<any> = [];
+  highPrices: Array<any> = [];
+  lowPrices: Array<any> = [];
 
-  constructor(private api: ApiService, private maService: MovingAverageService) { }
+  constructor(private api: ApiService, private maService: MovingAverageService) {
+    
+
+  }
 
   ngOnInit() {
-    this.api.getCandelstick().subscribe(d => {
-      console.log(d);
+    // this.api.getCandlestick().subscribe((d: Array<any>) => {
+    //   this.allDataPoints = (<any>d).reduce((datum, point, i, arr) => {
+    //     datum.push(point[2]);
+    //     // this.closePrices.push(point[3]);
+    //     // this.highPrices.push(point[4]);
+    //     // this.lowPrices.push(point[5]);
+    //     return datum;
+
+    //   }, []);
+    // });
+    this.api.getCandlestick().subscribe(d => {
+      console.log(d)
     })
-    
-    this.maLineY = this.maService.updatePrices(closePrices);
+
+    // this.maLineY = this.maService.updatePrices(this.closePrices);
     this.maLineX = this.maService.updateDates(dates);
-    
+
+
     this.graph = {
       data: [
         {
           // Price chart
-          x: dates, 
-          close: closePrices, 
-          decreasing: {line: {color: '#7F7F7F'}}, 
-          high: highPrices, 
-          increasing: {line: {color: '#17BECF'}}, 
-          line: {color: 'rgba(31,119,180,1)'}, 
-          low: lowPrices, 
-          open: openPrices, 
-          type: 'candlestick', 
-          xaxis: 'x', 
+          x: dates,
+          close: this.closePrices,
+          decreasing: { line: { color: '#7F7F7F' } },
+          high: this.highPrices,
+          increasing: { line: { color: '#17BECF' } },
+          line: { color: 'rgba(31,119,180,1)' },
+          low: this.lowPrices,
+          open: this.openPrices,
+          type: 'candlestick',
+          xaxis: 'x',
           yaxis: 'y'
         },
         {
           // Moving average
-          x: this.maLineX, 
+          x: this.maLineX,
           y: this.maLineY,
           type: 'scatter',
           xaxis: 'x',
-          yaxis: 'y', 
+          yaxis: 'y',
           mode: 'lines',
           line: { width: '1', },
         },
       ],
       layout: {
-        dragmode: 'zoom', 
+        dragmode: 'zoom',
         margin: {
-          r: 10, 
-          t: 25, 
-          b: 40, 
+          r: 10,
+          t: 25,
+          b: 40,
           l: 60
-        }, 
-        showlegend: false, 
+        },
+        showlegend: false,
         xaxis: {
-          autorange: true, 
-          domain: [0, 1], 
-          range: ['2017-01-03 12:00', '2017-02-15 12:00'], 
+          autorange: true,
+          domain: [0, 1],
+          range: ['2017-01-03 12:00', '2017-02-15 12:00'],
           // rangeslider: {range: ['2017-01-03 12:00', '2017-02-15 12:00']}, 
-          title: 'Date', 
+          title: 'Date',
           type: 'date'
-        }, 
+        },
         yaxis: {
-          autorange: true, 
-          domain: [0, 1], 
-          range: [114.609999778, 137.410004222], 
+          autorange: true,
+          domain: [0, 1],
+          range: [114.609999778, 137.410004222],
           type: 'linear'
         }
       }
     }
 
+  }
+  ngOnChanges(changes) {
+    console.log(changes)
   }
 
 }
