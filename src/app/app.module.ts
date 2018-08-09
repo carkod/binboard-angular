8812493e83147b969c64a38a9f11b8b993e220d0
carkod <carkodw@gmail.com>
2018-08-09 03:22:32 +0200

    autocomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
-import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule, MatAutocompleteModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -20,6 +20,7 @@ import { RoutesRoutingModule } from './routes/routes-routing.module';
 import { RoutesModule } from './routes/routes.module';
 import { StockPickerComponent } from './stock-picker/stock-picker.component';
 import { DrawerService } from './drawer.service';
+import { ReactiveFormsModule, FormsModule } from '../../node_modules/@angular/forms';
 
 // Angular router here
 // - /Dashboard
@@ -60,6 +61,10 @@ import { DrawerService } from './drawer.service';
     HttpClientModule,
     RoutesRoutingModule,
     RoutesModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    FormsModule
   ],
   providers: [CryptotableComponent, DatePipe, ApiService, DrawerService],
   bootstrap: [AppComponent]
--- src/app/stock-picker/stock-picker.component.ts
+++ src/app/stock-picker/stock-picker.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { FormControl, FormGroup } from '../../../node_modules/@angular/forms';
+import { Observable } from '../../../node_modules/rxjs';
+import { startWith, map, filter } from '../../../node_modules/rxjs/operators';
+
+export interface Ticker {
+  symbol: string, price: string
+}
 
 @Component({
   selector: 'stock-picker',
@@ -8,12 +15,26 @@ import { ApiService } from '../api.service';
 })
 export class StockPickerComponent implements OnInit {
 
+  coins: Array<Ticker>;
+  coinsForm = new FormControl();
+  filteredCoins: Observable<any>;
+
   constructor(private api: ApiService) { }
 
   ngOnInit() {
-    this.api.getCoins(100).subscribe(d => {
-      console.log(d);
-    })
+    this.api.getCoins(100).subscribe(coinData => {
+      this.coins = coinData;
+    });
+    this.filteredCoins = this.coinsForm.valueChanges.pipe(
+        startWith(''), map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string) {
+    const filterValue = value.toLowerCase();
+    return filterValue;
+    // return this.coins.pipe(map(res => res.filter(option => option.toLowerCase().includes(filterValue))));
+    // return this.coins.filter(option => option.toLowerCase().includes(filterValue));
   }
 
 }
