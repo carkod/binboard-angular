9cfcf34afb72e262ea76b1f8656199c005c910e1
carkod <carkodw@gmail.com>
2018-09-23 21:12:23 +0200

    adding predict field

--- client/app/stock-picker/stat-data/stat-data.component.ts
+++ client/app/stock-picker/stat-data/stat-data.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import * as tf from '@tensorflow/tfjs';
 import { ApiService } from '../../services/api.service';
 import { DbService } from '../../services/db.service';
+import { flatten } from '@tensorflow/tfjs-layers/dist/exports_layers';
 
 @Component({
   selector: 'stat-data',
@@ -33,16 +34,23 @@ export class StatDataComponent implements OnInit {
   async train() {
     // Define a model for linear regression.
     this.linearModel = tf.sequential();
-    this.linearModel.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+    // this.linearModel.add(flatten())
+    this.linearModel.add(tf.layers.dense({ units: 1, inputShape: [2] }));
 
     // Prepare the model for training: Specify the loss and the optimizer.
     this.linearModel.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
 
 
-    // Training data, completely random stuff
-    const xs = tf.tensor1d(this.data.closeTime);
-    const ys = tf.tensor1d(this.data.closePrices);
+    // Training data
+    const convertString = this.data.closePrices.map(Number);
+    const convertTimeToNumber = this.data.closeTime.map(date => date.getTime())
+    // const xs = tf.tensor2d([537628399999, 1537630199999, 1537631999999, 1537633799999], [50, 1]);
+    // const ys = tf.tensor2d([0.00238173, 0.0023151, 0.0023555, 0.00233285], [50, 1]);
 
+    const xs = tf.tensor(convertTimeToNumber);
+    const ys = tf.tensor(convertString);
+
+    console.log(xs, ys)
 
     // Train
     await this.linearModel.fit(xs, ys)
@@ -50,9 +58,14 @@ export class StatDataComponent implements OnInit {
     console.log('model trained!')
   }
 
-  predict(val: number) {
-    console.log(this.linearModel, val)
-    const output = this.linearModel.predict(tf.tensor2d([val], [1, 1])) as any;
-    this.prediction = Array.from(output.dataSync())[0]
+  predict(val: Array<number>) {
+    // const output = this.linearModel.predict(tf.tensor2d([val], [1, 1])) as any;
+    const currentTime = new Date().getTime();
+    const currentPrice = 0.024;
+    const predictArray = [currentTime, currentPrice]
+    const output = this.linearModel.predict(tf.tensor(predictArray)) as any;
+    console.log(Array.from(output.dataSync()))
+    // this.prediction = Array.from(output.dataSync())[0]
+    
   }
 }
