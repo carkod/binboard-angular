f52566ca793b9258ed730f5fcc4f8896a4af8a79
carkod <carkodw@gmail.com>
2018-11-02 22:33:40 +0100

    serve env config

--- server/src/config.js
+++ server/src/config.js
@@ -15,7 +15,6 @@ if (process.env.NODE_ENV !== 'production') {
   const dotenv = require('dotenv-safe')
   dotenv.load({
     path: path.join(__dirname, '../.env'),
-    sample: path.join(__dirname, '../.env.example')
   })
 }
 
