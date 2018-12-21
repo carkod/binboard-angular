0dba9efb6e08b62df77518c4e4f710cb7782c5e3
carkod <carkodw@gmail.com>
2018-12-21 17:42:36 +0100

    fix conflcits

--- server/src/services/cronjob/ticker24/index.js
+++ server/src/services/cronjob/ticker24/index.js
@@ -11,11 +11,7 @@ export function ticker24job() {
   const ticker24url = api.base + '/' + api.ticker24;
   console.log(process.env)
   // schedule tasks to be run on the server   
-<<<<<<< HEAD
-  cron.schedule("0 */30 * * *", function () {
-=======
   cron.schedule("0 */3 * * *", function () {
->>>>>>> dfe6442971d4eeb8edd1ee5b7f64b04711071f9a
     console.log('executing ticker24 cronjob')
     request(ticker24url, function (error, response, resBody) {
       console.log('error:', error); // Print the error if one occurred
